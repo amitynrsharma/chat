3f8215eed6a9714da7e8dc64fe23e1ccf451a562
amit sharma <76862381+amitynrsharma@users.noreply.github.com>
2022-08-06 13:05:43 +0530

    did some changes

--- controller/userController.js
+++ controller/userController.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
         cb(null, 'public/images')
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname));
     }
 })
 
